35d330c6e932f0bec8f60cf4c0be19e83d299d84
Md Sazid Uddin <sazid.uddin@aiub.edu>
2026-03-01 12:10:08 +0600

    JS intro

---
+++ lab6/script.js
@@ -0,0 +1,33 @@
+// Primitive types in JavaScript
+var a = 12.5; // number
+var b = "Hello"; // string
+var c = true; // boolean
+var d; // undefined
+var e = undefined;
+var f = null;
+typeof(e);
+typeof(f);
+var bigNum = 123456789012345;
+
+var empty = undefined;
+var empty = null;
+
+console.log(isNaN(bigNum));
+console.log(isNaN(b));
+
+var str = 'Hello world!';
+var str2 = "It's a nice day!";
+var str3 = 'She said, "Hello!"';
+var str4 = 'She say\'s, "Hello!"'; // escape character
+console.log(str.length);
+
+// var var = "This is a variable"; // This will cause an error because 'var' is a reserved keyword
+// var true;
+console.log(a);
+console.log(typeof(a));
+console.log(b);
+console.log(typeof(b));
+console.log(c);
+console.log(typeof(c));
+console.log(d);
+console.log(typeof(d));
